a0f20830d358241e478e082e15b44bda97e9731d
fukymaka <0280140@gmail.com>
2021-06-27 21:11:37 +0300

    init repository and add movement

---
+++ Assets/Source/BoundsControl.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+
+namespace Source
+{
+    public class BoundsControl : MonoBehaviour
+    {
+
+        private float camHeight;
+        private float camWidth;
+        private void Start()
+        {
+            var mainCamera = Camera.main;
+            camHeight = mainCamera.orthographicSize;
+            camWidth = camHeight * mainCamera.aspect;
+
+            Debug.Log(camHeight);
+            Debug.Log(camWidth);
+        }
+
+        private void Update()
+        {
+            if (Math.Abs(transform.position.y) > camHeight)
+            {
+                var pos = transform.position;
+
+                if (pos.y > 0)
+                    pos.y = -camHeight;
+                else
+                    pos.y = camHeight;
+
+                transform.position = pos;
+            }
+            
+            if (Math.Abs(transform.position.x) > Math.Abs(camWidth))
+            {
+                var pos = transform.position;
+                
+                if (pos.x > 0)
+                    pos.x = -camWidth;
+                else
+                    pos.x = camWidth;
+                
+                transform.position = pos;
+            }
+
+            
+        }
+    }
+}
---
+++ Assets/Source/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+namespace Source
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField] private float moveSpeed = 1f;
+        [SerializeField] private float rotationSpeed = 360f;
+        private Rigidbody2D _rigidbody;
+
+        private void Start()
+        {
+            _rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            Move();
+            Rotate();
+        }
+
+        private void Move()
+        {
+            if (Input.GetKey(KeyCode.W)) 
+                _rigidbody.AddForce(transform.up * moveSpeed);
+        }
+
+        private void Rotate()
+        {
+            float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+            rotation *= Time.deltaTime;
+            transform.Rotate(0, 0, -rotation);
+        }
+    }
+}
